6623896be3a1feb2440875fb8ae3bed0f6098267
unzinc <46981693+unzinc@users.noreply.github.com>
2024-06-07 10:18:48 -0600

    day1.py

--- day1.py
+++ day1.py
@@ -3,16 +3,35 @@ Created on Thu Jun  6 14:34:42 2024
 
 @author: KDM
 """
-
+import re
 #Create some blank arrays
 nums=[]
 nums_cut=[]
 
+#num dict set up this way for any instances of joined number words eg: eightwo, twone
+num_dict={'one':'o1ne',
+          'two':'t2wo',
+          'three':'t3ree',
+          'four':'f4our',
+          'five':'f5ive',
+          'six':'s6ix',
+          'seven':'s7even',
+          'eight':'e8ight',
+          'nine':'n9ine'}
+
+#Function to replace string numbers to digits
+def replace_text_numbers(text, mapping):
+    for ent in mapping:
+        text=text.replace(ent, mapping[ent])
+    return text
+
 #open file and filter out the letters
 with open('input.txt', 'r') as file:
     for line in file:
-        nums.append(''.join(filter(str.isdigit, line)))
+        mod_text=replace_text_numbers(line, num_dict)
+        nums.append(''.join(filter(str.isdigit, mod_text)))
         #print(''.join(filter(str.isdigit, line)))
+        
         
 #Check number length and add first and last number to nums_cut array
 for num in nums:
